abd730a6df4eba526b135ab6548ef95c6fe832ee
Artur Uvarov <artur.uff@gmail.com>
2024-06-30 09:18:44 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Artur Uvarov on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
